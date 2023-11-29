0c4c3530de7aab4dd282b40e4440a243800c1255
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-11-29 16:58:25 +0800

    Adds handler for generic ok responses

---
+++ api/src/common/genericOkResponse.ts
@@ -0,0 +1,9 @@
+import { Response } from "express";
+
+export default function genericOkResponse(
+  res: Response,
+  body?: any,
+  message?: string
+) {
+  return res.status(200).json({ status: 200, message, body });
+}
