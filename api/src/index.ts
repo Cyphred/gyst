9f6e2ff925fe95822c64819caa89e6d88ce95623
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-11-29 15:37:10 +0800

    Adds basic index logic

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,9 @@
 import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
 
+// Load .env data
 dotenv.config();
 
 // Grab port number from .env
@@ -7,4 +11,50 @@ dotenv.config();
 let port = parseInt(process.env.PORT);
 if (!port || isNaN(port)) port = 8000;
 
-console.log("Using port", port);
+// Attempt to connect to MongoDB server
+// Stop the program if unable to
+try {
+  console.log(
+    `Attempting to connect to MongoDB at ${process.env.MONGO_URI}...`
+  );
+  await mongoose.connect(process.env.MONGO_URI);
+} catch (err) {
+  console.error(err);
+  console.error(
+    `Could not connect to MongoDB server at ${process.env.MONGO_URI}`
+  );
+  process.exit(1);
+}
+
+// Create express app
+const app = express();
+
+// Enables parsing of incoming data into json
+app.use(express.json());
+
+// Trust the proxy
+app.set("trust proxy", true);
+
+// Logs incoming requests to console
+app.use((req, res, next) => {
+  console.log(req.path, req.method, req.ips);
+  next();
+});
+
+// IMPORTANT NOTE
+// Change the origin to the domain during production
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+// Load in root router
+// app.use("/api", rootRouter);
+
+// Start listening for requests
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
+
+export default app;
