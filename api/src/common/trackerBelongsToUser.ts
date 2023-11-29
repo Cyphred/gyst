23e9b4ccfe222b31593fc71ff66557cc3a63e335
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-11-29 17:55:40 +0800

    Adds function for checking if a tracker belongs to a user

---
+++ api/src/common/trackerBelongsToUser.ts
@@ -0,0 +1,18 @@
+import ExpenseTrackerModel from "../models/expenseTracker.js";
+
+/**
+ * Checks if a tracker belongs to the provided user.
+ * @param trackerId The `_id` of the expense tracker
+ * @param userId The `_id` of the user making the request
+ * @returns `true` if the tracker belongs to the user. `false` if not.
+ */
+export default async (trackerId: string, userId: string) => {
+  const tracker = await ExpenseTrackerModel.findOne({
+    _id: trackerId,
+    user: userId,
+  });
+
+  if (!tracker) return false;
+
+  return true;
+};
