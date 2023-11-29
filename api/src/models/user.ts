7addc860dbff65c6b8941757fbd5fe7e34bf4236
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-11-29 18:38:52 +0800

    Adds static signup method to user model

--- api/src/models/user.ts
+++ api/src/models/user.ts
@@ -1,4 +1,7 @@
 import mongoose, { Schema, Model, Types, Document } from "mongoose";
+import ApiError from "../errorHandlers/apiError.js";
+import { ErrorCode } from "../errorHandlers/errorCodes.js";
+import bcryptjs from "bcryptjs";
 
 export interface User {
   username: string;
@@ -13,7 +16,14 @@ export interface User {
 
 export interface UserDocument extends User, Document {}
 
-const UserSchema: Schema<UserDocument> = new Schema<UserDocument>(
+interface UserModelInterface extends Model<UserDocument> {
+  signup(userInfo: User): Promise<UserDocument>;
+}
+
+const UserSchema: Schema<UserDocument, UserModelInterface> = new Schema<
+  UserDocument,
+  UserModelInterface
+>(
   {
     username: {
       type: String,
@@ -42,9 +52,43 @@ const UserSchema: Schema<UserDocument> = new Schema<UserDocument>(
   { timestamps: true }
 );
 
-const UserModel: Model<UserDocument> = mongoose.model<UserDocument>(
-  "User",
-  UserSchema
-);
+/**
+ * Creates a user account
+ * @param userInfo The account info of the user
+ * @returns The newly-created user account
+ */
+UserSchema.statics.signup = async function (
+  userInfo: User
+): Promise<UserDocument> {
+  const { username, password, name } = userInfo;
+  const UserModel = this as Model<UserDocument>;
+
+  // Fetch a user with the same username
+  const existingUser = await UserModel.findOne({ username });
+
+  // Reject if the username is already taken
+  if (existingUser) throw new ApiError(ErrorCode.USERNAME_IN_USE);
+
+  // Fetch the salt rounds from .env and generate a salt
+  const saltRounds = parseInt(process.env.SALT_ROUNDS);
+  const salt = await bcryptjs.genSalt(saltRounds);
+
+  // Hash and salt the password
+  const hash = await bcryptjs.hash(password, salt);
+
+  // Create the user
+  const user = await UserModel.create({
+    username,
+    password: hash,
+    name,
+  });
+
+  return user;
+};
+
+const UserModel: UserModelInterface = mongoose.model<
+  UserDocument,
+  UserModelInterface
+>("User", UserSchema);
 
 export default UserModel;
