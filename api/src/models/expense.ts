c9dca270191dfa921e339b6612f0d6a6a370ce63
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-11-29 16:37:40 +0800

    Adds timestamp fields for expense

--- api/src/models/expense.ts
+++ api/src/models/expense.ts
@@ -1,6 +1,13 @@
 import mongoose, { Schema, Types, Document, Model } from "mongoose";
 
 export interface Expense {
+  /**
+   * Not to be confused with the `createdAt` and `updatedAt` properties of the document.
+   *
+   * `date` is the intended/actual date an expense has occurred. This may or may not be
+   * on the same date that the expense was logged.
+   */
+  date: Date;
   amount: number;
   description: string;
   notes?: string[];
@@ -10,31 +17,38 @@ export interface Expense {
 
 export interface ExpenseDocument extends Expense, Document {}
 
-const ExpenseSchema: Schema<ExpenseDocument> = new Schema<ExpenseDocument>({
-  amount: {
-    type: Number,
-    required: true,
-    validate: {
-      validator: function (amount: number) {
-        return amount >= 0;
+const ExpenseSchema: Schema<ExpenseDocument> = new Schema<ExpenseDocument>(
+  {
+    date: {
+      type: Date,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      validate: {
+        validator: function (amount: number) {
+          return amount >= 0;
+        },
+        message: "Amount cannot be a negative value",
       },
-      message: "Amount cannot be a negative value",
+    },
+    description: { type: String, required: true },
+    notes: {
+      type: [String],
+    },
+    tags: {
+      type: [Schema.Types.ObjectId],
+      ref: "ExpenseTag",
+    },
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: "ExpenseCategory",
+      required: true,
     },
   },
-  description: { type: String, required: true },
-  notes: {
-    type: [String],
-  },
-  tags: {
-    type: [Schema.Types.ObjectId],
-    ref: "ExpenseTag",
-  },
-  category: {
-    type: Schema.Types.ObjectId,
-    ref: "ExpenseCategory",
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const ExpenseModel: Model<ExpenseDocument> = mongoose.model<ExpenseDocument>(
   "Expense",
